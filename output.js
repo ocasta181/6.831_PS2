ec003d214064f1b8a043b692a88935d1d989f6b1
ocasta181 <ocasta181@gmail.com>
2015-02-28 20:45:22 -0500

    Added kings, move arrows, and turn indicator
    user input moves is the next step.

--- output.js
+++ output.js
@@ -42,6 +42,23 @@ var toggleTurn = function(color) {
         console.log("got here!")
         whoseTurn = "black";
     }
+
+    setTurnDisplay(whoseTurn);
+    //ctx.rotate(180);
+
+}
+
+var setTurnDisplay = function(color){
+
+    $("#turn_display").text(function(){
+        return color.charAt(0).toUpperCase() + color.slice(1) + " Turn";
+    });
+    if(color == "black"){
+        var textColor = "white"; 
+    } else {
+        var textColor = "black";
+    };
+    $("#turn_display").css({"background-color": color, "color": textColor});
 }
 
 var initializeBoard = function(board) {
@@ -60,8 +77,15 @@ var initializeBoard = function(board) {
     };
     red_checker = new Image();
     black_checker = new Image();
+    red_king = new Image();
+    black_king = new Image();
     red_checker.src = 'graphics/red-piece.png';
     black_checker.src = 'graphics/black-piece.png';
+    red_king.src = 'graphics/red-king.png';
+    black_king.src = 'graphics/black-king.png';
+
+
+
 };
 
 var refreshPeices = function(board){
@@ -71,13 +95,68 @@ var refreshPeices = function(board){
         var checker = checkers[i];
         var x = checker.col*square_size;
         var y = checker.row*square_size;
-        if(checker.color == "red"){
-            ctx.drawImage(red_checker, x, y, square_size, square_size);
+        if(checker.isKing){
+            if(checker.color == "red"){
+                ctx.drawImage(red_king, x, y, square_size, square_size);
+            } else {
+                ctx.drawImage(black_king, x, y, square_size, square_size);
+            };
         } else {
-            ctx.drawImage(black_checker, x, y, square_size, square_size);
-        };
+            if(checker.color == "red"){
+                ctx.drawImage(red_checker, x, y, square_size, square_size);
+            } else {
+                ctx.drawImage(black_checker, x, y, square_size, square_size);
+            };
+        }
     };
+    console.log("did a board refresh");
 };
+
+
+
+var markLastMove = function(result){
+
+    var toCenterDim = function(pos){
+        return (pos*square_size)+(square_size/2);
+    };
+
+    start_x = toCenterDim(result.from_col);
+    start_y = toCenterDim(result.from_row);
+    end_x = toCenterDim(result.to_col);
+    end_y = toCenterDim(result.to_row);
+};
+
+var lastMoveIndicator = function(){
+
+    ctx.strokeStyle = "yellow";
+    ctx.lineWidth = square_size*.1;
+    ctx.beginPath();
+    ctx.moveTo(start_x, start_y);
+    ctx.lineTo(end_x,end_y);
+    ctx.stroke();
+
+    var moveScalar_x = start_x - end_x;
+    var moveScalar_y = start_y - end_y;
+    var moveDirection_x = moveScalar_x?moveScalar_x<0?-1:1:0;
+    var moveDirection_y = moveScalar_y?moveScalar_y<0?-1:1:0;
+
+    if(start_x == end_x){
+        ctx.beginPath();
+        ctx.moveTo(end_x+(square_size*.3),end_y+(square_size*moveDirection_y*.3));
+        ctx.lineTo(end_x,end_y);
+        ctx.lineTo(end_x-(square_size*.3),end_y+(square_size*moveDirection_y*.3));
+        ctx.stroke();
+        
+    } else {
+        ctx.beginPath();
+        ctx.moveTo(end_x,end_y+(square_size*moveDirection_y*.3));
+        ctx.lineTo(end_x,end_y);
+        ctx.lineTo(end_x+(square_size*moveDirection_x*.3),end_y);
+        ctx.stroke();
+
+    }
+    console.log("indicated last move");
+}
 
 
 
@@ -97,40 +176,38 @@ $(document).ready(function() {
     
 
 
-
+    var refreshBoard = function(){
+        ctx.clearRect(0,0,400,400);
+        initializeBoard(board);
+        refreshPeices(board);
+        //lastMoveIndicator();
+    };
  	
 
     board.addEventListener('add',function (e) {
-        ctx.clearRect(0,0,400,400);
-        initializeBoard(board);
-		refreshPeices(board);
+        refreshBoard();
 	},true);
 
 	board.addEventListener('move',function (e) {
-        ctx.clearRect(0,0,400,400);
-        initializeBoard(board);
-		refreshPeices(board);
-        console.log(whoseTurn);
+        refreshBoard();
 
 	},true);
 
     board.addEventListener('remove', function(e) {
-        ctx.clearRect(0,0,400,400);
-        initializeBoard(board);
-    	refreshPeices(board);
+        refreshBoard();
     }, true);
 
     board.addEventListener('promote',function (e) {
-        ctx.clearRect(0,0,400,400);
-        initializeBoard(board);
-		refreshPeices(board);
 	},true);
+
+    //ctx.
 
     
     $("#btnNewGame").click(function(evt) {
 
         board.prepareNewGame();
-        refreshPeices(board);
+        //refreshBoard();
+        setTurnDisplay(whoseTurn);
     });
 
     $("#btnAutoMove").click(function(evt) {
@@ -138,8 +215,11 @@ $(document).ready(function() {
       var playerDirection = directionOf(playerColor);
       var result = rules.makeRandomMove(playerColor, playerDirection);
       if (result != null) {
+        console.log("hit auto move")
+        markLastMove(result);
         toggleTurn();
       }
+        lastMoveIndicator();
     });
 
     board.prepareNewGame();
